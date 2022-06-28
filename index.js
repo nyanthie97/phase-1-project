b9ecfce673bf83aaa5eaf1086458ede7c69b3217
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-06-28 05:44:25 +0300

    all changes in three of thems

--- index.js
+++ index.js
@@ -54,23 +54,18 @@ function displayDescriptions(beer) {
             isRed = false
         }
 
+        var post = document.getElementById("post");
+        post.addEventListener("click", function () {
+            var commentBoxValue = document.getElementById("comment-box").value;
+
+            var li = document.createElement("li");
+            var text = document.createTextNode(commentBoxValue);
+            li.appendChild(text);
+            document.getElementById("unordered").appendChild(li);
+
+        });
 
     })
 }
 
-$(document).ready(function () {
-    $('#comment').click(function () {
-        var input = $('#input').val();
-        $('.box').append(input + '<br>');
-        $('#input').val('');
-        $('.box-containers').slideDown();
-    });
-    $('#cancel').click(function () {
-        $('#input').val('');
-    })
-    $('#delete').click(function () {
-        $('.box').text('');
-        $('.box-containers').slideUp();
-    })
-});
 
